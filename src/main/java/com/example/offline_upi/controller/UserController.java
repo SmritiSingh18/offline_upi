f4cb83eae50c2184825bc5777a255763fee7718e
Smriti Singh <smritisingh1818@gmail.comz>
2026-06-28 20:37:56 +0530

    Implement user registration API with controller and service

---
+++ src/main/java/com/example/offline_upi/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.offline_upi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.offline_upi.dto.RegisterUserRequest;
+import com.example.offline_upi.dto.UserResponse;
+import com.example.offline_upi.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    UserController(UserService userService){
+        this.userService=userService;
+    }
+    @PostMapping("/register")
+    public UserResponse registerUsere(@RequestBody RegisterUserRequest request) {
+       
+        return userService.registerUser(request);
+    }
+    
+}
